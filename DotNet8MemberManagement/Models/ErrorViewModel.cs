e63f85b200531910944a2513ad9b7bb06aca4807
i_nakabayashi <i_nakabayashi@pac.ne.jp>
2025-07-04 16:56:26 +0900

    InitialCommit

---
+++ DotNet8MemberManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNet8MemberManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
